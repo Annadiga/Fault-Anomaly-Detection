7c9baa7f0dca7a8d449ec4f8436321f11472a36a
Lisa <lisaburini.98@gmail.com>
2023-03-21 23:23:21 +0100

    normalized time in each topic

---
+++ preprocessing.m
@@ -0,0 +1,44 @@
+%% ----------------------------------------------------- 
+close all;
+clear all;
+clc;
+
+%% Input .mat file
+% Please change the path to a sequence .mat file from the dataset
+filename = 'processed_MAT_Files/carbonZ_2018-07-18-15-53-31_1_engine_failure.mat';
+
+%% Add the dataset tools library to the path
+addpath('alfa-tools');
+
+%% Load the sequence through the constructor
+Sequence = sequence(filename);
+
+%% Print brief information about the sequence
+Sequence.PrintBriefInfo();
+
+%% For each topic in topics
+topics = fieldnames(Sequence.Topics);
+% Get the start time to normalize times to start from zero
+start_time = Sequence.GetStartTime();
+
+for i = 1:numel(topics)
+    % Get the topic name
+    topic_name = topics(i);
+
+    % Assign data to variable topic 
+    topic = Sequence.GetTopicByName(topic_name{1});
+
+    % Normalize the time stamps in the topic
+    times = topic.Data.time_recv - start_time;
+    
+    % get data
+    data = topic.Data;
+
+    % Add column "times" with normalized time
+    data.times = times;
+    if i == 2
+        break
+    end
+end
+
+
