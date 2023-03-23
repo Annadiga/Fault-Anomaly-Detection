df861c3c2fa87240e6f3157f81c23caa230688da
Annadiga <57714440+Annadiga@users.noreply.github.com>
2023-03-23 13:25:38 +0100

    selection of some variables for preprocessing and updates to gitignore

--- preprocessing.m
+++ preprocessing.m
@@ -6,13 +6,13 @@ clc;
 %% CREAZIONE TABELLA FINALE CON ATTRIBUTI DI INTERESSE
 % var1 | var2 | var 3 | ... | fault_label
 %dataTable = table('Size',[1 1], 'VariableTypes', {'int8'}, 'VariableNames',{'FaultLabel'});
-dataTable = table('Size',[1 4], 'VariableTypes', {'timetable','timetable','timetable','int8'}, 'VariableNames',{'s_errVel_xTT', 's_errVel_yTT', 's_errVel_zTT' ,'FaultLabel'});
+%dataTable = table('Size',[1 4], 'VariableTypes', {'timetable','timetable','timetable','int8'}, 'VariableNames',{'s_errVel_xTT', 's_errVel_yTT', 's_errVel_zTT' ,'FaultLabel'});
 %dataTable = table('Size',[1 3], 'VariableTypes', {'timetable','timetable','timetable'}, 'VariableNames',{'s_errVel_xTT', 's_errVel_yTT', 's_errVel_zTT'});
-
+dataTable = table();
 
 
 freq_sampling = 25;
-formatIn = 'ss.SSSSSSSSSSSSSSS'; % define the input format for seconds with fractional values
+%formatIn = 'ss.SSSSSSSSSSSSSSS'; % define the input format for seconds with fractional values
 
 %% 
 % METTEREMO CICLO ESTERNO
@@ -48,7 +48,7 @@ for i = 1:numel(topics)
     % Get the topic name
     topic_name = topics(i);
 
-    if isequal(topic_name{1}, 'mavros_imu_data_raw') || isequal(topic_name{1}, 'mavros_imu_mag')
+    if isequal(topic_name{1}, 'mavros_imu_data_raw') || isequal(topic_name{1}, 'mavros_imu_mag') || isequal(topic_name{1}, 'mavros_imu_atm_pressure') || isequal(topic_name{1}, 'mavros_global_position_compass_hdg') || isequal(topic_name{1}, 'mavctrl_rpy') || isequal(topic_name{1}, 'mavlink_from') ||  isequal(topic_name{1}, 'mavros_local_position_pose') || isequal(topic_name{1}, 'mavros_imu_data') || isequal(topic_name{1}, 'mavros_wind_estimation') || isequal(topic_name{1}, 'mavros_setpoint_raw_target_global') || isequal(topic_name{1}, 'mavros_imu_temperature') 
         continue
     end
 
@@ -96,24 +96,66 @@ for i = 1:numel(topics)
 
         % Sampling timestamps in the topic for 25 Hz
         s_errVel_xTT = resample(errVel_xTT, freq_sampling);
+        s_errVel_xTT = renamevars(s_errVel_xTT, 'Var1', 's_errVel_x');
+
         s_errVel_yTT = resample(errVel_yTT, freq_sampling);
+        s_errVel_yTT = renamevars(s_errVel_yTT, 'Var1', 's_errVel_y');
+
         s_errVel_zTT = resample(errVel_zTT, freq_sampling);
+        s_errVel_zTT = renamevars(s_errVel_zTT, 'Var1', 's_errVel_z');
+
 
         subplot(2,1,1)
         plot(errVel_xTT.timestamps,errVel_xTT.Var1,'-o')
         subplot(2,1,2)
-        plot(s_errVel_xTT.Time, s_errVel_xTT.Var1,'-o')
+        plot(s_errVel_xTT.Time, s_errVel_xTT.s_errVel_x,'-o')
 
 
-        dataTable.s_errVel_xTT(j) = s_errVel_xTT;
-        dataTable.s_errVel_yTT(j) = s_errVel_yTT;
-        dataTable.s_errVel_zTT(j) = s_errVel_zTT;
+
+        dataTable.s_errVel_xTT(j) = {s_errVel_xTT};
+        dataTable.s_errVel_yTT(j) = {s_errVel_yTT};
+        dataTable.s_errVel_zTT(j) = {s_errVel_zTT};
 
 
     end
 
 
-    if i == 4
+    if isequal(topic_name{1}, 'mavros_global_position_global')
+        timestamps = seconds(data.times);
+        % Create timetables
+        altitudeTT = timetable(timestamps, data.altitude);
+        latitudeTT = timetable(timestamps, data.latitude);
+        longitudeTT = timetable(timestamps, data.longitude);
+        
+
+        % Sampling timestamps in the topic for 25 Hz
+        s_altitudeTT = resample(altitudeTT, freq_sampling);
+        s_altitudeTT = renamevars(s_altitudeTT, 'Var1', 's_altitute');
+
+        s_latitudeTT = resample(latitudeTT, freq_sampling);
+        s_latitudeTT = renamevars(s_latitudeTT, 'Var1', 's_latitude');
+
+        s_longitudeTT = resample(errVel_zTT, freq_sampling);
+        s_longitudeTT = renamevars(s_longitudeTT, 'Var1', 's_longitude');
+
+
+        subplot(2,1,1)
+        plot(altitudeTT.timestamps,altitudeTT.Var1,'-o')
+        subplot(2,1,2)
+        plot(s_altitudeTT.Time, s_altitudeTT.s_altitute,'-o')
+
+
+
+        dataTable.s_altitudeTT(j) = {s_altitudeTT};
+        dataTable.s_latitudeTT(j) = {s_latitudeTT};
+        dataTable.s_longitudeTT(j) = {s_longitudeTT};
+
+
+
+
+    end
+
+    if i == 14
         break
     end
 end
